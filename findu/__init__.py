0c6a22ce15da8d88fcd6ee5efd192ec26f004165
MarkHoo <markhoo911@gmail.com>
2023-06-07 13:27:04 +0800

    update: Release 0.1.0

--- findu/__init__.py
+++ findu/__init__.py
@@ -1,5 +1,21 @@
-from .findu import add as add
+""" Sherlock Module
+
+This module contains the main logic to search for usernames at social
+networks.
+
+"""
+
+import sys
+
+# Check if the user is using the correct version of Python
+python_version = sys.version.split()[0]
+
+if sys.version_info < (3, 6):
+    print("Sherlock requires Python 3.6+\nYou are using Python %s, which is not supported by Sherlock" % (python_version))
+    sys.exit(1)
 
 
-__all__ = ["add"]
-__version__ = "0.0.3"
+from .findu import *
+# import findu
+findu.main()
+
